212027315c8e0011eb233e1e5f7fc0868308c92f
sven <sven.tan@icloud.com>
2023-05-06 18:43:00 +0800

    fix logos size

--- components/logosBlock.tsx
+++ components/logosBlock.tsx
@@ -9,7 +9,6 @@ const ImageLogo = ({ src, width = 120, height = 16, alt = "" }) => {
       alt={alt}
       width={width}
       height={height}
-      style={{ width: 'auto', height: 'auto' }}
       className="mx-auto my-8"
     />
   );
